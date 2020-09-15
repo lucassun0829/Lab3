018d35f17d968d2e2ca073d0c9c6702c2f62c3bf
lucassun0829 <lks5713@psu.edu>
2020-09-15 15:33:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# Author: Lucas Sun lks5713@psu.edu
+# Collaborator: Yousef Alzaabi yaa5136@psu.edu
+# Collaborator: Maksim Zingman mfz5130@psu.edu
+# Collaborator:
+# Section: 2
+# Breakout: 3
+def sum_n(n): 
+  if n>0:
+    return n+sum_n(n-1)
+  else:
+    return 0
+
+def print_n(s,n):
+  if n>0:
+    print(s)
+    print_n(s,n-1)
+  else:
+    return  
+    
+def run():
+  num = int(input("Enter a int:"))
+  sum = sum_n(num)
+  print("sum is "+str(sum))
+  s = input("Enter a String:")
+  print(print_n(s,num))
+
+if __name__ =="__main__":
+  run()
